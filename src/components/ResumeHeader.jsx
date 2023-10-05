0661bcba946cb23038aff8fcc2d8b0e46951bbb6
gwongibeom <k201gun@kakao.com>
2023-10-06 08:34:35 +0900

    연락처 수정

--- src/components/ResumeHeader.jsx
+++ src/components/ResumeHeader.jsx
@@ -2,7 +2,7 @@ import './ResumeHeader.css'
 import profile_normal from '../assets/profile_normal.png'
 import profile_something_on_your_mind from '../assets/profile_something_on_your_mind.png'
 import MP3something_on_your_mind from '../assets/MP3something_on_your_mind.mp3'
-import { BsTelephoneFill, BsDiscord, BsInstagram } from 'react-icons/bs'
+import { BsTelephoneFill, BsDiscord, BsGithub } from 'react-icons/bs'
 import { useState } from 'react'
 
 function ResumeHeader() {
@@ -23,6 +23,7 @@ function ResumeHeader() {
   return (
     <div className='resumeHeader'>
       <img
+        className='profile'
         src={profile}
         onClick={changeProfile}
         alt='반뿔테 안경을 쓰고 착하게 생긴 권기범의 프로필 사진(컨퍼런스 배경)'
@@ -33,19 +34,19 @@ function ResumeHeader() {
           <div className='phoneNumber contacts'>
             <BsTelephoneFill id='icon' /> <a href='tel:+82 1043989956'>01043989956</a>
           </div>
-
           <div className='contacts'>
-            <BsDiscord id='icon' /> <p>ggb.</p>
+            <BsGithub id='icon' />
+            <a href='https://github.com/gwongibeom'>gwongibeom</a>
+          </div>
+          <div className='contacts'>
+            <BsDiscord id='icon' />
+            <p>ggb.</p>
           </div>
           <div className='contacts'>
             <p id='icon' alt='디스콰이엇 로고'>
               D*
-            </p>{' '}
+            </p>
             <a href='https://disquiet.io/@_ggbdev_'>권기범</a>
-          </div>
-          <div className='contacts'>
-            <BsInstagram id='icon' />{' '}
-            <a href='https://www.instagram.com/gwon_gi_beom0_0/'>gwon_gi_beom0_0</a>
           </div>
         </div>
       </div>
